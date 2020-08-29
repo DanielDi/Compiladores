3dbbfbde6a6e92d0e6243f8ffc38dabf424f319f
Sebastian Lopez Restrepo <selopezre@unal.edu.co>
2020-08-29 16:10:35 -0500

    Metodos de vistas

--- bpmn_parser/ensayo.py
+++ bpmn_parser/ensayo.py
@@ -1,7 +1,7 @@
 import numpy as nppip 
 import pandas as pd
 import easygui as eg
-from parse_funcion import get_evento_inicio
+from parse_funcion import *
 
 extension = ["*.txt","*.csv"]
 pd.options.display.max_columns = None
@@ -13,8 +13,10 @@ def main():
                          filetypes=extension)
     data = pd.read_csv(archivo)
     data.dropna(axis=1,how='all',inplace=True)
-    print(data)
-    print(get_evento_inicio(data))
+    #print(data)
+    #print(get_evento_inicio(data))
+    #print(get_grupos_vistas(data))
+    print(on_vista(data,get_grupos_vistas(data).id))
 
 if __name__ == "__main__": 
     main() 
--- bpmn_parser/parse_funcion.py
+++ bpmn_parser/parse_funcion.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def get_evento_inicio(df):
     return (df[(df['Nombre']=='Evento de inicio') & (df['tipo']=='Inicio')].dropna(axis=1))
+
+def get_grupos_vistas(df):
+    return (df[(df['Nombre']=='Grupo')].dropna(axis=1))
+
+def on_vista(df,vista):
+    return (df[df['Contenido por'] == float(vista)].dropna(axis=1))
