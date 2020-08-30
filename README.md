68ea3706617603ac631a9e757c0922034ee6c247
Santiago Marín <esmarina@unal.edu.co>
2020-08-29 23:50:33 -0500

    Clave secreta del proyecto django escondida

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ Para instalar por primera vez:
  - ubicarse en la carpeta del proyecto: pipenv install
 
  Para correr la app web "poke_app":
+ - Añadir el archivo secrets.py en la carpeta poke_app/poke_app el cual contiene la clave secreta del proyecto django
  - Ubicarse en la carpeta poke_app
  - Ejecutar el comando: py manage.py runserver
  - Ingresar con el navegador a http://localhost:8000
  - Verificar que aparece la página de bienvenida de django
- 
+
  Nota:
  Fijarse si es en el ambiente generado por pipenv.
  Si esta usando visual studio code 
--- poke_app/poke_app/settings.py
+++ poke_app/poke_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from . import secrets
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8@ai71j5wl1=r)jk$7mav+fyml5@0%5qa=bcp9z2a#%ea-lx+j'
+SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
