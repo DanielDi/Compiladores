14698756820fadbffcc15791487ea5714cd9a6c4
Santiago Marín <esmarina@unal.edu.co>
2020-08-31 12:45:44 -0500

    vista 'index' modificada para permitir la carga de archivos

--- poke_app/apps/index/views.py
+++ poke_app/apps/index/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  if request.method == 'POST' and request.FILES['myFile']:
+    myfile = request.FILES['myFile']
+    fs = FileSystemStorage()
+    filename = fs.save(myfile.name, myfile)
+    uploaded_file_url = fs.url(filename)
+    return render(request, 'index.html', { 'uploaded_file_url': uploaded_file_url })
+
+  else:
+    return render(request, 'index.html')
