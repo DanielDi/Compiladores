92feacd3120aed9cad32af33800b18fb1f851e78
Santiago Marín <esmarina@unal.edu.co>
2020-09-13 22:14:01 -0500

    Genera botón y formulario a partir de vista_poke2.csv

--- poke_app/apps/index/ensayo.py
+++ poke_app/apps/index/ensayo.py
@@ -8,16 +8,9 @@ extension = ["*.txt","*.csv"]
 pd.options.display.max_columns = None
 
 def main(archivo): 
-    # archivo = eg.fileopenbox(msg="Abrir archivo",
-    #                      title="Control: fileopenbox",
-    #                      default='',
-    #                      filetypes=extension)
     archivo = ('diagramas/'+archivo)
     archivo = os.path.abspath(archivo)
     data = pd.read_csv(archivo)
-    print(data)
     data = parse_funcion.depurar(data)
     data.dropna(axis=1,how='all',inplace=True)
-    print(data,'\n')
-    print(parse_funcion.add_origen_destino(data))
     data.to_json(r'json.json', orient='records') #Crear json
--- poke_app/apps/index/views.py
+++ poke_app/apps/index/views.py
@@ -2,10 +2,32 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect
 from . import ensayo
+import json
+import os
 
 # Create your views here.
 
+def crearFormulario(form_json, idForm):
+  formulario = "<h3>" + form_json['Área de texto 1'] + "</h3>" + "<form action=''>"
+  print("Mi form_json", form_json)
+  campos = (form_json['campos']).split(';')
+  id_etiqueta = 1
+  for campo in campos:
+    nombre, tipo = campo.split(':')
+    label = "<label for='" + idForm + "." + str(id_etiqueta) + "'>" + nombre + "</label><br>"
+    entrada = "<input type='" + tipo + "' id='" + idForm + "." + str(id_etiqueta) + "'><br>"
+    formulario += label + entrada
+    id_etiqueta += 1
+
+  formulario += "<br><input type='submit' value='Enviar'></form>"
+  return formulario
+
+def crearBoton(boton_json, idBoton):
+  boton = "<button type='button' id='" + idBoton + "'>" + boton_json['Área de texto 1'] + "</button>"
+  return boton
+
 def index(request):
+  context = {}
   if request.method == 'POST' and request.FILES['myFile']:
     myfile = request.FILES['myFile']    # Este es el archivo que se va a depurar
     fs = FileSystemStorage()
@@ -15,21 +37,47 @@ def index(request):
     filename = fs.save(myfile.name, myfile)   # fs.save(nombre, contenido)
 
     ensayo.main(filename)
-    print("Archivo:", filename, " cargado con éxito")
-    return HttpResponseRedirect("/")
+
+    ruta_json = os.path.abspath('json.json') 
+
+    mi_json = open(ruta_json, 'r').read()
+    mi_json = json.loads(mi_json)
+    
+    print(mi_json)  
+
+    numForm = 0
+    numBoton = 0
+
+    for obj in mi_json:
+      if (obj['tipo'] == 'formulario'):
+        numForm += 1
+        idForm = 'formulario' + str(numForm)
+        context[idForm] = crearFormulario(obj, idForm)
+
+      if (obj['tipo'] == "boton"):
+        numBoton += 1
+        idBoton = 'boton' + str(numBoton)
+        context[idBoton] = crearBoton(obj, idBoton)
+
+
+    print("Num formularios:", numForm)
+    print("Num botones:", numBoton)
+    # context['titulo'] = "<h1>Mi header</h1>"
+
+    # context['formulario'] = """
+    #   <form action="/action_page.php">
+    #   <label for="fname">First name:</label><br>
+    #   <input type="text" id="fname" name="fname" value="John"><br>
+    #   <label for="lname">Last name:</label><br>
+    #   <input type="text" id="lname" name="lname" value="Doe"><br><br>
+    #   <input type="submit" value="Submit">
+    # """
+
+    # render(request, 'index.html', context)
+    # return HttpResponseRedirect("/")
 
   # Diccionario que se le envía a la plantilla, para imprimir su valor sobre esta se hace por
   # medio de la clave
-  context = {}
-  context['titulo'] = "<h1>Mi header</h1>"
-
-  context['formulario'] = """
-    <form action="/action_page.php">
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" value="John"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="Doe"><br><br>
-    <input type="submit" value="Submit">
-  """
+  
 
   return render(request, 'index.html', context)
