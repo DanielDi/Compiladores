4a38a71c648205e2f1eb1ef0c6db2e87f8496b03
Sebastian Lopez Restrepo <selopezre@unal.edu.co>
2020-09-27 18:57:41 -0500

    Añadidos nuevos tipos al formulario
    Daniel y sebas trabajaron en esta tarea

--- poke_app/apps/index/funciones_html.py
+++ poke_app/apps/index/funciones_html.py
@@ -6,11 +6,26 @@ def crearFormulario(form_json, idForm):
   campos = (form_json['campos']).split(';')
   id_etiqueta = 1
   for campo in campos:
+    entrada = ""
     nombre, tipo = campo.split(':')
-    label = "<label for='" + idForm + "." + str(id_etiqueta) + "'>" + nombre + "</label><br>"
-    entrada = "<input type='" + tipo + "' id='" + idForm + "." + str(id_etiqueta) + "'><br>"
+    label = "<label for='" + idForm + "." + str(id_etiqueta) + "'>" + nombre +":" + "</label><br>"
+    #radio(pollo,res,cerdo,humano)
+    #Verificar si tipo de elemento es radio y checkbox. de caso contrario lo hace noraml
+    if "(" in tipo:
+      tipo, opciones = tipo.split('(')
+      opciones = opciones[:-1].split(',')
+      for opcion in opciones:
+        entrada += ("<input type='" + tipo + "' id='" + idForm + "." + str(id_etiqueta) + 
+        "' name='"+ nombre +"' value='"+opcion+str(id_etiqueta)+"'>" + opcion)
+        id_etiqueta += 1
+      if tipo == "radio":
+        entrada = entrada[:(len(opcion)+1)*-1] + "required" + ">" + opcion + "<br>"
+      else:
+        entrada += "<br>"
+    else:    
+      entrada = "<input type='" + tipo + "' id='" + idForm + "." + str(id_etiqueta) + "'><br>"
+      id_etiqueta += 1
     formulario += label + entrada
-    id_etiqueta += 1
 
   formulario += "<br><input type='submit' value='Enviar'></form>"
   return formulario
