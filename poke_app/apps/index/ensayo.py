de0cd533181abf2eff1152b204c2cc0fea546e27
Daniel <despinalm@unal.edu.co>
2020-11-11 08:29:22 -0500

    Depuracion correcta

--- poke_app/apps/index/ensayo.py
+++ poke_app/apps/index/ensayo.py
@@ -13,4 +13,5 @@ def main(archivo):
     data = pd.read_csv(archivo)
     data = parse_funcion.depurar(data)
     print(parse_funcion.add_origen_destino(data))
+    print(data)
     data.to_json(r'json.json', orient='records') #Crear json
